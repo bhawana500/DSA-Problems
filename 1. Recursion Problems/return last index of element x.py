75d0356ce5916703a366e896e6702aff41e2bff8
Bhawana Rokey <Bhawanarokey@gmail.com>
2025-04-08 22:13:39 +0530

    Return the last index of given number

---
+++ 1. Recursion Problems/return last index of element x.py
@@ -0,0 +1,10 @@
+def check_list(x,l,start):
+    if start == len(l):
+        return False
+    if l[start] == x:
+        return start+len(l)
+    else:
+        return check_list(x,l,start-1)
+l=[1,2,3,4,2,5,7,6,7,8]
+x=int(input("Enter the element to find it's first occurence: "))
+print(check_list(x,l,-1))
