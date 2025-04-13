65520ad29b2f9388a735abebc428039c6b1b9ba8
Bhawana Rokey <Bhawanarokey@gmail.com>
2025-04-13 22:08:47 +0530

    find index of x using binary serach

--- 1. Recursion Problems/find_x.py
+++ 1. Recursion Problems/find_x.py
@@ -1,15 +1,15 @@
 def find(l,x,si,ei):
     if si>ei:
         return -1
-    k=(si+ei)//2
-    if l[k]==x:
-        return k
-    if l[k]>x:
-        return find(l,x,si,k-1)
+    mid=(si+ei)//2
+    if l[mid]==x:
+        return mid
+    if l[mid]>x:
+        return find(l,x,si,mid-1)
     else:
-        return find(l,x,k+1,ei)
+        return find(l,x,mid+1,ei)
 l=[1,2,3,4,5,6,7,8,22,44,56,88,100,2,9,10,1]
-x=1001
+x=3
 l.sort()
 print(l)
-print(find(l,x,0,len(l)-1)) 
+print(f"{x} index is",find(l,x,0,len(l)-1)) 
