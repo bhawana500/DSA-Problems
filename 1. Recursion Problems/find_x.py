82b86cf148f88509129befa595671ac7a39eee06
Bhawana Rokey <Bhawanarokey@gmail.com>
2025-07-21 14:22:02 +0530

    BInary Seach index find

--- 1. Recursion Problems/find_x.py
+++ 1. Recursion Problems/find_x.py
@@ -1,15 +1,13 @@
-def find(l,x,si,ei):
-    if si>ei:
+def binary_search(l,target,li,si):
+    mid=(li+si)//2
+    if li>si:
         return -1
-    mid=(si+ei)//2
-    if l[mid]==x:
+    if target==l[mid]:
         return mid
-    if l[mid]>x:
-        return find(l,x,si,mid-1)
+    elif target>l[mid]:
+        return binary_search(l,target,mid+1,si)
     else:
-        return find(l,x,mid+1,ei)
-l=[1,2,3,4,5,6,7,8,22,44,56,88,100,2,9,10,1]
-x=3
-l.sort()
-print(l)
-print(find(l,x,0,len(l)-1)) 
+        return binary_search(l[:mid],target,li,mid-1)
+
+l=[-9,0,1,3,6,9,12,56,78]                                             
+print(binary_search(l,12,0,len(l)-1))
